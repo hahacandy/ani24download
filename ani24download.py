fb718a3fac12527296eb32645fa1b9869b8cee5c
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-19 00:14:47 +0900

    Update ani24download.py

--- ani24download.py
+++ ani24download.py
@@ -330,10 +330,14 @@ class AniDownThread(QThread):
                             # 파일 덧 붙여서 저장 중
                             f.write(chunk)
 
-                            # 현재 파일 사이즈
                             current_size += len(chunk)
 
+                            print(current_size)
+
                             try:
+                                # 현재 파일 사이즈
+                                current_size += len(chunk)
+
                                 # 1초 마다 정보 변경
                                 current_time = time.perf_counter()
                                 if last_time + 1 <= current_time:
